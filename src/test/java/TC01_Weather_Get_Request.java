4ab428a534ded037a819104550b2d6c41c7d341e
Zubair-codoid <55594784+Zubair-codoid@users.noreply.github.com>
2021-01-08 13:01:28 +0530

    Initial commit

---
+++ src/test/java/TC01_Weather_Get_Request.java
@@ -0,0 +1,35 @@
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TC01_Weather_Get_Request {
+
+    @Test
+    void getWeatherDetails() {
+
+        //base URL
+        RestAssured.baseURI = "http://api.weatherapi.com/v1";
+
+        //Specify Request object
+        RequestSpecification httpRequest = RestAssured.given();
+
+
+        // Response object
+        Response response = httpRequest.request(Method.GET, "/current.json?key=9a399d13bf3443998c252903210801&q=London");
+
+
+        // Json to String
+//        String responseBody = response.getBody().asString();
+//        System.out.println("This is the response Body" + responseBody);
+
+        //Status code verification
+        int statusCode = response.getStatusCode();
+        System.out.println("The Status code is >>>>   " + statusCode);
+        Assert.assertEquals(statusCode, 200);
+
+
+    }
+}
