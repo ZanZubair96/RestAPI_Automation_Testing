b7001bd427de610c09122426fda79723a6865348
Zubair-codoid <55594784+Zubair-codoid@users.noreply.github.com>
2021-01-08 19:24:57 +0530

    Added the Status line Verification and updated API_KEY

--- src/test/java/TC01_Weather_Get_Request.java
+++ src/test/java/TC01_Weather_Get_Request.java
@@ -9,6 +9,7 @@ public class TC01_Weather_Get_Request {
 
     @Test
     void getWeatherDetails() {
+        String key = Constants.API_KEY;
 
         //base URL
         RestAssured.baseURI = "http://api.weatherapi.com/v1";
@@ -16,20 +17,21 @@ public class TC01_Weather_Get_Request {
         //Specify Request object
         RequestSpecification httpRequest = RestAssured.given();
 
-
         // Response object
-        Response response = httpRequest.request(Method.GET, "/current.json?key=9a399d13bf3443998c252903210801&q=London");
-
+        Response response = httpRequest.request(Method.GET, "/current.json?key="+key+"&q=London");
 
         // Json to String
-//        String responseBody = response.getBody().asString();
-//        System.out.println("This is the response Body" + responseBody);
+        String responseBody = response.getBody().asString();
+        System.out.println("This is the response Body" + responseBody);
 
         //Status code verification
         int statusCode = response.getStatusCode();
         System.out.println("The Status code is >>>>   " + statusCode);
         Assert.assertEquals(statusCode, 200);
 
-
+        //status line verification
+        String statusLine=response.getStatusLine();
+        System.out.println("Status line is:"+statusLine);
+        Assert.assertEquals(statusLine, "HTTP/1.1 200 OK");
     }
 }
